b6ac56c79eaad1f8d91d34cb6174eb7670090939
notGriffin <krima724@gmail.com>
2025-11-10 10:40:09 -0600

    code

---
+++ .github/Program#1.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+saying = ('"And yet the one that comforts even the sinner is a blessed soul indeed -Griffin"')
+
+
+GUI = tk.Tk()
+GUI.title("MY GUI")
+GUI.geometry("525x50")
+
+label = tk.Label(GUI, text=saying)
+label.pack()
+
+GUI.mainloop()
